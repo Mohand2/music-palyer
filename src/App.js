f9a1d59a8e75a7877a77091f0cb6afc750b448a9
moh <alloun.mohand@gmail.com>
2022-11-14 19:29:05 +0100

    Add buttons functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
@@ -6,14 +6,38 @@ import Song from './components/Song';
 import data from './data';
 
 function App() {
-  const [songs, setSongs] = useState(data());
-  const [currentSong, setcurrentSong] = useState(songs[0]);
+  const [songs] = useState(data());
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [currentSong, setcurrentSong] = useState(songs[currentSongIndex]);
+
+  useEffect(() => {
+    setcurrentSong(songs[currentSongIndex]);
+
+    return () => {};
+  }, [currentSongIndex, songs]);
+
+  function forwardSongHandler(CurrentSongIndex) {
+    if (currentSongIndex < songs.length - 1)
+      setCurrentSongIndex(
+        (prevCurrentSongIndex) => (prevCurrentSongIndex += 1)
+      );
+  }
+  function backSongHandler(CurrentSongIndex) {
+    if (currentSongIndex > 0)
+      setCurrentSongIndex(
+        (prevCurrentSongIndex) => (prevCurrentSongIndex -= 1)
+      );
+  }
 
   return (
     <div className="App">
-      {console.log(currentSong)}
+      {/* {console.log(currentSong)} */}
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        forwardSongHandler={forwardSongHandler}
+        backSongHandler={backSongHandler}
+        currentSongIndex={currentSongIndex}
+      />
     </div>
   );
 }
