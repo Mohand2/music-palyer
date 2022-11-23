4af2b7274fe65d0bfc386196128d70753778b8fe
moh <alloun.mohand@gmail.com>
2022-11-23 20:10:30 +0100

    add playlist song

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlaylistSong from './PlaylistSong';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-function Playlist({songs}) {
+function Playlist({ songs }) {
+  const [playlistIsShown, setPlaylistIsShown] = useState(false);
+
+  function togglePlaylist() {
+    setPlaylistIsShown((pIsShown) => !pIsShown);
+  }
+
   return (
     <div className="playlist">
-      {songs.map((song) => (
-        <PlaylistSong key={song.id} song={song} />
-      ))}
+      {!playlistIsShown && (
+        <FontAwesomeIcon
+          icon={faBars}
+          size="2x"
+          className="icon"
+          onClick={togglePlaylist}
+        />
+      )}
+      {playlistIsShown && (
+        <div>
+          <div div className="playlist-header">
+            <h1 className="title"> Playlist</h1>
+            <FontAwesomeIcon
+              className="icon"
+              size="2x"
+              icon={faArrowLeft}
+              onClick={togglePlaylist}
+            />
+          </div>
+
+          {songs.map((song) => (
+            <PlaylistSong key={song.id} song={song} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/PlaylistSong.js
+++ src/components/PlaylistSong.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function PlaylistSong({ song }) {
+  const { name, artist, cover } = song;
+
   return (
-    <div className="song">
-      <h3>{song.name}</h3>
-      <h4>{ }</h4>
-    </div>
+    <>
+      <div className="song">
+        <div className="song-leading">
+          <h3>{name}</h3>
+          <p>{artist}</p>
+        </div>
+        <div className="song-trailing">
+          <img src={cover} alt="" />
+        </div>
+      </div>
+    </>
   );
 }
 
